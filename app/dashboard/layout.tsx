ad73d638f55a52691892d7073faa6dac8d429ef0
Ankan Bhattacharjee <ankanb2006@gmail.com>
2026-04-13 15:32:19 +0530

    fix: global page overlap by switching to AnimatePresence wait mode

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -52,7 +52,7 @@ export default function DashboardLayout({
       <div className="flex-1 flex flex-col overflow-hidden relative z-10">
         <Topbar />
         <main className="flex-1 overflow-y-auto overflow-x-hidden p-4 md:p-6 lg:p-8">
-          <AnimatePresence mode="popLayout" initial={false}>
+          <AnimatePresence mode="wait" initial={false}>
             <PageTransition key={pathname}>
               {children}
             </PageTransition>
